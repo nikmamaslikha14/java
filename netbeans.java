df4e25124e91900946c07c66e1fb6b25968e15f2
nikmamaslikha14 <70304862+nikmamaslikha14@users.noreply.github.com>
2020-08-27 10:13:24 +0700

    Add files via upload

---
+++ netbeans.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tutorial;
+
+/**
+ *
+ * @author ANI
+ */
+public class netbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("nama siswa : nikmatul maslikha");
+        System.out.println("jenis kelamin : perempuan");
+        System.out.println("alamat : gunung sari kertosari");
+        System.out.println("jurusan : rekayasa perangkat lunak");
+    }
+    
+}
